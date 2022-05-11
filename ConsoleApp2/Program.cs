db402bfb8881fc6043d0f0bcbeec3b07244290a3
Funke Ajibola <arikemi406@gmail.com>
2022-05-11 15:47:44 +0100

    changes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,80 +11,39 @@ namespace ConsoleApp2
     public  class Program
     {
         //Task.Run
-       public static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //Task[] t = new Task[2];
-            // string[] files = null;
-            // string[] dir = null;
+           var total = calculator.Add(50, 50);
+            Console.WriteLine(total);
+            var subtotal = calculator.Add(new List<int>{ 5,6,7,8,4,6,78,3,5,2,300});
+            Console.WriteLine(subtotal);
 
-            // string docdirectory = Environment.GetFolderPath(Environment.SpecialFolder.System);
-            // t[0] = Task.Factory.StartNew(() => files = Directory.GetFiles(docdirectory));
-            // t[1] = Task.Factory.StartNew(() => dir = Directory.GetDirectories(docdirectory));
-
-            // Task.Factory.ContinueWhenAny(t, completedfile =>
-            // {
-            //     Console.WriteLine("{0}::Contains", docdirectory);
-            //     Console.WriteLine("{0}::files", files.Length);
-            //     Console.WriteLine("{0}::subdirectories", dir.Length);
-            // });
-            //Console.ReadKey();
-            Console.Write("Enter path: ");
-            
-            string path = Console.ReadLine();
-            if (path == null)
-            {
-                Console.WriteLine("Invalid input");
-            }
-            else
-            {
-                if (Directory.Exists(path))
-                {
-                    Directory.Delete(path);
-                    Console.WriteLine("Folder deleted");
-                }
-                else
-                {
-                    Console.WriteLine("Folder not found");
-                }
-            }
-
-            
         }
-        //public static void calculate()
-        //{
-        //    Task.Run(() =>
-        //        calculate1()
-        //    );
-            
-        //    Task.Run(() =>
-        //        calculate2()
-        //    );
-        //   Task.Run(() =>
-        //        calculate3()
-        //   );
 
-        //    Console.Read();
-        //}
-
-
-        //public static int calculate1()
-        //{
-        //    Thread.Sleep(3000);
-        //    Console.WriteLine("caculation 1 in progress");
-        //    return 500;
-        //}
-        //public static int calculate2()
-        //{
-        //    Thread.Sleep(2000);
-        //    Console.WriteLine("caculation 2 in progress");
-        //    return 2100;
-        //}
-        //public static int calculate3()
-        //{
-        //    Console.WriteLine("caculation 3 in progress");
-        //    return 100;
-        //}
-        
     }
-   
+
+    
+    public class calculator
+    {
+        [Obsolete("use :Add(List<int> numbers) method ")]
+        public static int Add(int firstnumber, int secondnumber)
+        {
+            Console.WriteLine("method outdated"); 
+            return firstnumber + secondnumber;
+           
+        }
+
+        public static int Add(List<int> numbers)
+        {
+            Console.WriteLine("new method");
+            int result = 0;
+            foreach(var number in numbers)
+            {
+                result = result + number;
+            }
+            return result;
+        }
+    }
+
+
 }
