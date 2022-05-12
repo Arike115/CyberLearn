d39229bca7f8069a0427fe6a3d06b24cdbcf8596
Funke Ajibola <arikemi406@gmail.com>
2022-05-12 15:08:35 +0100

    File and System.IO

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,16 +10,81 @@ namespace ConsoleApp2
 {
     public  class Program
     {
-        //Task.Run
+        
         public static void Main(string[] args)
         {
-           var total = calculator.Add(50, 50);
-            Console.WriteLine(total);
-            var subtotal = calculator.Add(new List<int>{ 5,6,7,8,4,6,78,3,5,2,300});
-            Console.WriteLine(subtotal);
+            //Directoryinfo
+            DirectoryInfo dir = new DirectoryInfo(".");
+            DirectoryInfo direc = new DirectoryInfo(@"C:\Program Files");
+            Console.WriteLine(direc.Name);
+            Console.WriteLine(direc.FullName);
+            Console.WriteLine(direc.Parent);
+            Console.WriteLine(direc.CreationTime);
+            Console.WriteLine(dir.Name);
+
+            //File
+
+            string[] words = { "this is a ball", "this is a house", "set to chalk", "mice" };
+            string path = @"C:\testingFile.txt";
+
+            File.WriteAllLines(path, words);
+
+            foreach(var word in File.ReadLines(path))
+            {
+                Console.WriteLine(word);
+            }
+
+            //fileinfo
+
+            //DirectoryInfo dit = new DirectoryInfo(@"Documents");
+
+            //FileInfo[] textfile = dit.GetFiles("*.txt", SearchOption.AllDirectories);
+            //Console.WriteLine("matches:{0}", textfile.Length);
+
+            //foreach (FileInfo file in textfile)
+            //{
+            //    Console.WriteLine(file.FullName);
+            //    Console.WriteLine(file.CreationTime);
+
+            //}
+            Writeforall();
+            Readforall();
+
+            //filestream write
+            string path3 = @"C:\testingFile.txt";
+            FileStream fs = File.Open(path3, FileMode.OpenOrCreate);
+          //or  var fx = new FileStream(path3, FileMode.Open);
+            string neword = "this is a brand new car";
+
+             byte[] bytlist = Encoding.Default.GetBytes(neword);
+            fs.Write(bytlist, 0, bytlist.Length);
+            fs.Position = 0;
+
+            byte[] filelist = new byte[bytlist.Length];
+            for(int i = 0; i < bytlist.Length; i++)
+            {
+                filelist[i] = (byte)fs.ReadByte();
+            }
+            Console.WriteLine(Encoding.Default.GetString(filelist));
+            fs.Close();
 
         }
 
+        //streamwriter
+        public static void Writeforall()
+        {
+            StreamWriter sw = new StreamWriter(@"C:\testingFile.txt");
+            sw.WriteLine("welcome to cyberspace");
+            sw.WriteLine("pleasure to meet you");
+            sw.Close();
+        }
+
+        public static void Readforall()
+        {
+            StreamReader sr = new StreamReader(@"C:\testingFile.txt");
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+        }
     }
 
     
